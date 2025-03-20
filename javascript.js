f6f77709c2c450bee6b12539aea40f71ae24d228
Alef-Henrique <alefsax7@outlook.com>
2025-03-20 20:33:20 -0300

    Atualização da responsividade.

--- javascript.js
+++ javascript.js
@@ -59,6 +59,7 @@ let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
 let overlay = document.querySelector('.overlay-menu')
 let btnFechar = document.getElementById('btn'); // Botão de fechar
+let btnFecharSessao = document.getElementById('btn-fechar-lista')
 
 btnMenu.addEventListener('click', () => {
     menu.classList.add('abrir-menu');
@@ -74,3 +75,7 @@ overlay.addEventListener('click', () => {
     menu.classList.remove('abrir-menu');
     overlay.style.display = "none"; 
 });
+btnFecharSessao.addEventListener('click',() =>{
+    menu.classList.remove('abrir-menu');
+    overlay.style.display = "none";
+})
